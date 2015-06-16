1ecaeb68c13537aab318c8406e265af45f38da2b
Elviro Rocca <elviro.rocca@facile.it>
2015-06-16 16:29:36 +0200

    Initial Commit

---
+++ Lazy/Lazy.h
@@ -0,0 +1,19 @@
+//
+//  Lazy.h
+//  Lazy
+//
+//  Created by Elviro Rocca on 16/06/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lazy.
+FOUNDATION_EXPORT double LazyVersionNumber;
+
+//! Project version string for Lazy.
+FOUNDATION_EXPORT const unsigned char LazyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lazy/PublicHeader.h>
+
+
